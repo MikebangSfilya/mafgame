95dbf621cf6d0ff4fd1aa7fe850c7c2af1cc9f11
Sfilya <Typegoot@gmail.com>
2026-06-21 03:42:19 +0300

    feat: complete basic day loop

--- cmd/main.py
+++ cmd/main.py
@@ -3,15 +3,22 @@ from game_rules.day_turn import DayTurn
 
 def main():
     game = DayTurn()
-    game.start_day()
+    print(game.start())
     while True:
-        input_comma = input("Введите команду (new_turn / end_day / quit): ")
-        if input_comma == 'quit':
+        command = input("Command (wait / confirm / next / quit): ")
+        if command == "quit":
             break
-
-        if not game.day_started:
-            game.start_day()
-        game.new_turn(input_comma)
+        try:
+            if command == "wait":
+                game.assign_orders(["wait", "wait"])
+            elif command == "confirm":
+                print(game.confirm_day().content)
+            elif command == "next":
+                print(game.next_day())
+            else:
+                print(f"Unknown command: {command}")
+        except RuntimeError as error:
+            print(error)
 
 if __name__ == "__main__":
     main()
